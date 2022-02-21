cfc3783568c1820eae43ab91f0ab1cbf815f8309
Omega_Assassin <94875433+Omega-Assassin@users.noreply.github.com>
2022-02-21 11:24:24 -0800

    Create hardermastermind.py
    Only 8 tries!

---
+++ hardermastermind.py
@@ -0,0 +1,60 @@
+import random
+print("welcome to mastermind only one of each color")
+colist=["red","green","yellow","blue","white","black"]
+print("Colours: ", colist)
+x=colist[random.randrange(0,5)]
+y=colist[random.randrange(0,5)]
+while y==x:
+    y=colist[random.randrange(0,5)]
+z=colist[random.randrange(0,5)]
+while y==z or x==z:
+    z=colist[random.randrange(0,5)]
+a=colist[random.randrange(0,5)]
+while a==z or a==y or a==x:
+    a=colist[random.randrange(0,5)]
+password=[x,y,z,a]
+b=input("First colour: ")
+c=input("Second colour: ")
+d=input("Third colour: ")
+e=input("Fourth colour: ")
+scolist=[]
+i=0
+while (b!=x or c!=y or d!=z or e!=a):
+    if b==x:
+        scolist.append("black")
+    elif password.count(b)==1:
+        scolist.append("white")
+    else:
+        scolist.append("none")
+    if c==y:
+        scolist.append("black")
+    elif password.count(c)==1:
+        scolist.append("white")
+    else:
+        scolist.append("none")
+    if d==z:
+        scolist.append("black")
+    elif password.count(d)==1:
+        scolist.append("white")
+    else:
+        scolist.append("none")
+    if e==a:
+        scolist.append("black")
+    elif password.count(e)==1:
+        scolist.append("white")
+    else:
+        scolist.append("none")
+    random.shuffle(scolist)
+    print(scolist)
+    scolist.clear()
+    i+=1
+    print("Attempt: ",i)
+    if i==8:
+        print("You lose")
+        break
+    b=input("First colour: ")
+    c=input("Second colour: ")
+    d=input("Third colour: ")
+    e=input("Fourth colour: ")
+else:
+    print("Congrats! You have guessed the code")
